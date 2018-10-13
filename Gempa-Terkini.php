54ffb4cbf56f11a08e7aeb6b8e4b952ffabaa611
adearta <39181028+adearta@users.noreply.github.com>
2018-10-13 13:11:54 +0700

    Update Gempa-Terkini.php

--- Gempa-Terkini.php
+++ Gempa-Terkini.php
@@ -3,7 +3,37 @@
 // Data Gempa Terkini
 // Coded By Rafsanzani Suhada
 // ZeroByte.ID
+require_once('./line_class.php');
+require_once('./unirest-php-master/src/Unirest.php');
 
+$channelAccessToken = 'xlBy/oeCeY2/qEiGiIZvSMOfRVJEasTAcMmwtKgLYS5wr6lvCAJ+NYsXrETcoFKLcLSs9NeJOBI51FxplgRErnpToxqom0D68ifQlRpufL+bFtMDzL5Ihk96TYs1xM95OTM3J0Wa5Lz+Q8WdaXFvPgdB04t89/1O/w1cDnyilFU=
+'; //sesuaikan 
+$channelSecret = 'f4d8b2c45d18b226e4531a48e993b6bd';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$groupId 	= $client->parseEvents()[0]['source']['groupId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$type 		= $client->parseEvents()[0]['type'];
+
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+
+$profil = $client->profil($userId);
+
+$pesan_datang = explode(" ", $message['text']);
+
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+}
+#-------------------------[Function]-------------------------#
 function main(){
 	$url = "http://data.bmkg.go.id/autogempa.xml";
 	$get = file_get_contents($url, False);
